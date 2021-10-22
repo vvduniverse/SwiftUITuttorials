d863a41a89099cbc31c8ac803523270229686768
Vahtee Boo <ronnie.kaa@bigmir.net>
2021-10-22 11:28:58 +0300

    Initial Commit

---
+++ SwiftUITuttorials/SwiftUITuttorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITuttorialsApp.swift
+//  SwiftUITuttorials
+//
+//  Created by Vahtee Boo on 22.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITuttorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
